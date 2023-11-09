11d8410f4b45f6656939e55104aa3e6e96799e7f
BingBangBrine <121695492+BingBangBrine@users.noreply.github.com>
2023-11-09 10:48:48 -0800

    copied basic vector field plotter from online

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
-# This is a sample Python script.
+# Import required modules
+import numpy as np
+import matplotlib.pyplot as plt
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Meshgrid
+x, y = np.meshgrid(np.linspace(-5, 5, 10),
+                   np.linspace(-5, 5, 10))
 
+# Directional vectors
+u = -y / np.sqrt(x ** 2 + y ** 2)
+v = x / (x ** 2 + y ** 2)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# Plotting Vector Field with QUIVER
+plt.quiver(x, y, u, v, color='g')
+plt.title('Vector Field')
 
+# Setting x, y boundary limits
+plt.xlim(-7, 7)
+plt.ylim(-7, 7)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# Show plot with grid
+plt.grid()
+plt.show()
